361ed4bcd33d52e963a9998332c49a05c35ccda9
Gonzalo Olvera Monroy <olvera9302@outlook.com>
2021-11-01 20:56:57 -0600

    [FIX] Project
    Faltaba la librarías Shortbread y Google

--- app/src/main/java/org/shop/thewarehouse/view/NavigationActivity.kt
+++ app/src/main/java/org/shop/thewarehouse/view/NavigationActivity.kt
@@ -6,12 +6,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.firestore.FirebaseFirestore
 import org.shop.thewarehouse.R
 import org.shop.thewarehouse.databinding.ActivityMainNavigationBinding
